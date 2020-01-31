47fb422ed7eb394704ea47ec5dcff8f634afb83b
tomas f <46322567+TomFern@users.noreply.github.com>
2020-01-31 15:29:29 -0300

    fix rails tests in 05-

--- drkiq/test/controllers/pages_controller_test.rb
+++ drkiq/test/controllers/pages_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+
+    # tutorial
+    # get pages_home_url
+    get "/"
     assert_response :success
+
   end
 
 end
--- drkiq/test/jobs/counter_job_test.rb
+++ drkiq/test/jobs/counter_job_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CounterJobTest < ActiveJob::TestCase
+
+  # tutorial
   # test "the truth" do
   #   assert true
   # end
+  test "returns 42" do
+     assert_equal 42, CounterJob.perform_now
+  end
+
 end
