704cecb90b248f59de3d08e1826b17818fd86079
tomas f <46322567+TomFern@users.noreply.github.com>
2020-01-31 13:00:37 -0300

    06- add production dockerfile, fix 04-add nginx and hosts allow

--- drkiq/config/environments/development.rb
+++ drkiq/config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # tutorial
+  # allow request from docker containers
+  config.hosts << "drkiq"
 end
